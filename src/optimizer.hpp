e762292abc8b1786fe7d7805722929593fc0a25f
Andrew Collins Foster <foster.ac@gmail.com>
2012-11-15 17:46:54 +0100

    Added Simulation based problem that uses RBF surrogates.  ATTN: A lot fo tuning is needed to get the optimizations to stop at the correct places, as the scales vary immensely.

--- src/optimizer.hpp
+++ src/optimizer.hpp
@@ -74,7 +74,8 @@ public:
 	    //Set the stop conditions
 	    //this requires some attention
 	    opt.set_xtol_rel(tolerance);
-	    opt.set_ftol_abs(tolerance);
+	    //opt.set_ftol_rel(tolerance);
+        //opt.set_ftol_abs(tolerance);
 
 	    //Pass a scalarized function through the 
 	    //Nlopt Adapter to the Nlopt object
