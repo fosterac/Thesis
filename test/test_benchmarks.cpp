53285847c75b4cac0423dd9683984088fd5eb796
Andrew Collins Foster <foster.ac@gmail.com>
2012-12-28 23:32:20 +0100

    Added FON problem to benchmark suite.

--- test/test_benchmarks.cpp
+++ test/test_benchmarks.cpp
@@ -10,6 +10,20 @@ namespace {
 
     //Problem::Interface * P = Problem::Factory("SURROGATE", 3, 2);
 
+    TEST(HOMOTOPY, DISABLED_FON){
+        Problem::Interface * P = Problem::Factory("FON", 2, 3);
+
+        //Set up the communication framework
+        Homotopy::Communication::SimulatedRemote< functionSet_t > Comm( P->Objectives );
+
+		//Instantiate the homotopy
+        Pareto::homotopy h( P, 1e-3, 1e-6, Comm );
+        h.fd_type = FiniteDifferences::FORWARD;
+
+		//Homotopically deform the ansatz
+		h.GetFront(15, 1, 0, 1);
+	}
+
 	TEST(HOMOTOPY, DISABLED_MOTTA1){
         Problem::Interface * P = Problem::Factory("MOTTA1", 3, 3);
 
@@ -63,7 +77,7 @@ namespace {
 		h.GetFront(10, 5, 0, 1);
 	}
 
-    TEST(HOMOTOPY, WFG5){
+    TEST(HOMOTOPY, DISABLED_WFG5){
         Problem::Interface * P = Problem::Factory("WFG5", 3, 3);
         /*
         std::vector< std::vector< double > > Design;
@@ -119,7 +133,7 @@ namespace {
 		h.GetFront(15, 5, 0, 1);
 	}
 
-    TEST(HOMOTOPY, DISABLED_DTLZ2){
+    TEST(HOMOTOPY, DTLZ2){
         Problem::Interface * P = Problem::Factory("DTLZ2", 3, 10);
 
         std::vector< std::vector< double > > Design;
